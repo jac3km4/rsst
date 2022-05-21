3ee1fd19b0d4cdffe82538f9db1021ba8313dad3
jekky <11986158+jac3km4@users.noreply.github.com>
2022-05-21 01:51:31 +0100

    More feature complete client

--- src/client.rs
+++ src/client.rs
@@ -1,43 +1,106 @@
 use std::error::Error;
 use std::fmt;
+use std::str::{FromStr, Utf8Error};
 
-use generic_async_http_client::Request;
+use hyper::body::Bytes;
+use hyper::client::HttpConnector;
+use hyper::http::uri::InvalidUri;
+use hyper::{Body, Client, Response, StatusCode, Uri};
 use ouroboros::self_referencing;
 use roxmltree_serde::{from_doc, Document, ParseError as XmlParseError};
 
 use crate::feed::Feed;
 
+#[cfg(feature = "rustls")]
+type Connector = hyper_rustls::HttpsConnector<HttpConnector>;
+#[cfg(not(feature = "rustls"))]
+type Connector = HttpConnector;
+
+#[derive(Debug)]
+pub struct RssClient {
+    client: Client<Connector, Body>,
+}
+
+impl RssClient {
+    const MAX_REDIRECT: usize = 10;
+
+    pub async fn exec(&self, mut req: RssRequest) -> Result<RssResponse<Bytes>, RssError> {
+        let mut redir_count = 0;
+        loop {
+            let origin = req.uri.clone();
+            let resp = self
+                .client
+                .get(req.uri)
+                .await
+                .map_err(RssError::HttpStreamError)?;
+
+            if resp.status().is_success() {
+                let body = hyper::body::to_bytes(resp.into_body())
+                    .await
+                    .map_err(RssError::HttpStreamError)?;
+                return RssResponse::from_bytes(body);
+            } else if resp.status().is_redirection() {
+                if redir_count > Self::MAX_REDIRECT {
+                    return Err(RssError::TooManyRedirects);
+                } else if let Some(redir_uri) = Self::redirect_uri(&origin, &resp) {
+                    req = RssRequest { uri: redir_uri };
+                    redir_count += 1;
+                } else {
+                    return Err(RssError::UnexpectedResponse(resp.status()));
+                }
+            } else {
+                return Err(RssError::UnexpectedResponse(resp.status()));
+            }
+        }
+    }
+
+    fn redirect_uri<A>(origin: &Uri, resp: &Response<A>) -> Option<Uri> {
+        let loc = resp.headers().get("Location")?;
+        let mut uri_parts = Uri::from_str(loc.to_str().ok()?).ok()?.into_parts();
+        if let (None, Some(scheme)) = (&uri_parts.scheme, origin.scheme()) {
+            uri_parts.scheme = Some(scheme.clone());
+        }
+        if let (None, Some(authority)) = (&uri_parts.authority, origin.authority()) {
+            uri_parts.authority = Some(authority.clone());
+        }
+        uri_parts.try_into().ok()
+    }
+}
+
+impl Default for RssClient {
+    fn default() -> Self {
+        #[cfg(feature = "rustls")]
+        let connector = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_or_http()
+            .enable_http1()
+            .enable_http2()
+            .build();
+        #[cfg(not(feature = "rustls"))]
+        let connector = HttpConnector::new();
+
+        let client = Client::builder().build(connector);
+        Self { client }
+    }
+}
+
 #[derive(Debug)]
 pub struct RssRequest {
-    req: Request,
+    uri: Uri,
 }
 
 impl RssRequest {
-    pub fn new(url: &str) -> Result<Self, RssError> {
-        let req = Request::new("GET", url).map_err(RssError::Http)?;
-        Ok(Self { req })
-    }
-
-    pub async fn exec(self) -> Result<RssResponse, RssError> {
-        let mut resp = self.req.exec().await.map_err(RssError::Http)?;
-
-        if (200..300).contains(&resp.status_code()) {
-            let body = resp.text().await.map_err(RssError::Http)?;
-            RssResponseTryBuilder {
-                body: body.into_boxed_str(),
-                document_builder: |str| Document::parse(str).map_err(RssError::XmlParse),
-                feed_builder: |doc| from_doc(doc).map_err(RssError::XmlDecode),
-            }
-            .try_build()
-        } else {
-            Err(RssError::UnexpectedResponse(resp.status_code()))
-        }
+    pub fn new(str: &str) -> Result<Self, RssError> {
+        let uri = str
+            .parse()
+            .map_err(|e: InvalidUri| RssError::HttpError(e.into()))?;
+        Ok(Self { uri })
     }
 }
 
 #[self_referencing]
-pub struct RssResponse {
-    body: Box<str>,
+pub struct RssResponse<A: 'static> {
+    body: A,
     #[borrows(body)]
     #[covariant]
     document: Document<'this>,
@@ -46,11 +109,14 @@ pub struct RssResponse {
     pub feed: Feed<'this>,
 }
 
-impl RssResponse {
-    pub fn from_string(str: String) -> Result<Self, RssError> {
+impl<A: AsRef<[u8]>> RssResponse<A> {
+    pub fn from_bytes(body: A) -> Result<Self, RssError> {
         RssResponseTryBuilder {
-            body: str.into_boxed_str(),
-            document_builder: |str| Document::parse(str).map_err(RssError::XmlParse),
+            body,
+            document_builder: |body| {
+                let str = std::str::from_utf8(body.as_ref()).map_err(RssError::EncodingError)?;
+                Document::parse(str).map_err(RssError::XmlParse)
+            },
             feed_builder: |doc| from_doc(doc).map_err(RssError::XmlDecode),
         }
         .try_build()
@@ -59,19 +125,25 @@ impl RssResponse {
 
 #[derive(Debug)]
 pub enum RssError {
-    Http(generic_async_http_client::Error),
-    UnexpectedResponse(u16),
+    HttpError(hyper::http::Error),
+    HttpStreamError(hyper::Error),
+    EncodingError(Utf8Error),
+    UnexpectedResponse(StatusCode),
     XmlDecode(roxmltree_serde::Error),
     XmlParse(XmlParseError),
+    TooManyRedirects,
 }
 
 impl fmt::Display for RssError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            RssError::Http(err) => write!(f, "http error: {err}"),
+            RssError::HttpError(err) => write!(f, "http error: {err}"),
+            RssError::HttpStreamError(err) => write!(f, "http error: {err}"),
+            RssError::EncodingError(err) => write!(f, "encoding error: {err}"),
             RssError::UnexpectedResponse(code) => write!(f, "unexpected response code {code}"),
             RssError::XmlDecode(err) => write!(f, "xml decode error: {err}"),
             RssError::XmlParse(err) => write!(f, "xml parse error: {err}"),
+            RssError::TooManyRedirects => write!(f, "too many redirects"),
         }
     }
 }
--- tests/parsing.rs
+++ tests/parsing.rs
@@ -4,6 +4,6 @@ fn parse_examples() {
     for file in std::fs::read_dir("./tests/examples").unwrap() {
         println!("{:?}", file);
         let contents = std::fs::read_to_string(file.unwrap().path()).unwrap();
-        rsst::client::RssResponse::from_string(contents).unwrap();
+        rsst::client::RssResponse::from_bytes(contents).unwrap();
     }
 }
